2164062380b456e797ab495e8d8b07d387af7402
barryodev <barryodev@gmail.com>
2023-03-17 21:38:10 +0000

    filling out full form

--- background.js
+++ background.js
@@ -1,20 +1,30 @@
-chrome.action.onClicked.addListener(async function(tab) {
-    let full_doc, input_field, box_model, input_field_x, input_field_y
-    
-    await chrome.debugger.attach({ tabId: tab.id }, "1.3")
+async function simulate_mouse_click(tab, target_x, target_y){
+    await chrome.debugger.sendCommand({ tabId: tab.id }, "Input.dispatchMouseEvent", {
+        type: "mousePressed",
+        x: target_x,
+        y: target_y,
+        button: "left"
+    })
 
+    await chrome.debugger.sendCommand({ tabId: tab.id }, "Input.dispatchMouseEvent", {
+        type: "mouseReleased",
+        x: target_x,
+        y: target_y,
+        button: "left"
+    })
+}
+
+async function clickElement(tab, selector) {
+    let full_doc, input_field, box_model, input_field_x, input_field_y
 
     full_doc = await chrome.debugger.sendCommand({ tabId: tab.id }, "DOM.getDocument", {
         depth: -1
     })
-    console.log('Got full doc node id: ' + full_doc.root.nodeId);
 
     input_field = await chrome.debugger.sendCommand({tabId: tab.id }, "DOM.querySelector", {
         nodeId: full_doc.root.nodeId,
-        selector: 'input[name="lastName"]'
+        selector: selector
     })
-
-    console.log('Got node id of input field: ' + input_field);
 
     box_model = await chrome.debugger.sendCommand({tabId: tab.id }, "DOM.getBoxModel", {
         nodeId: input_field.nodeId
@@ -23,27 +33,51 @@ chrome.action.onClicked.addListener(async function(tab) {
     input_field_x = box_model.model.content[0] + 20
     input_field_y = box_model.model.content[1] + 20
 
-    console.log('Box model of input field: ' + box_model.model.content);
+    simulate_mouse_click(tab, input_field_x, input_field_y)
+}
 
-    await chrome.debugger.sendCommand({ tabId: tab.id }, "Input.dispatchMouseEvent", {
-        type: "mousePressed",
-        x: input_field_x,
-        y: input_field_y,
-        button: "left"
-    })
+async function typeString(tab, wordToType) {
+    for (var i = 0; i < wordToType.length; i++) {
+        await chrome.debugger.sendCommand({ tabId: tab.id }, "Input.dispatchKeyEvent", {
+            type: "char",
+            text: wordToType[i],
+            modifiers: 0
+        })
+      }
+}
 
-    await chrome.debugger.sendCommand({ tabId: tab.id }, "Input.dispatchMouseEvent", {
-        type: "mouseReleased",
-        x: input_field_x,
-        y: input_field_y,
-        button: "left"
-    })
 
-    await chrome.debugger.sendCommand({ tabId: tab.id }, "Input.dispatchKeyEvent", {
-        type: "char",
-        text: "A",
-        modifiers: 0
-    })
+chrome.action.onClicked.addListener(async function(tab) {
+    
+    await chrome.debugger.attach({ tabId: tab.id }, "1.3")
+
+    await clickElement(tab, 'input[name="lastName"]')
+
+    await typeString(tab, "ODev")
+
+    await clickElement(tab, 'input[name="firstName"]')
+
+    await typeString(tab, "Barry")
+
+    await clickElement(tab, 'input[name="middleName"]')
+
+    await typeString(tab, "middle")
+
+    await clickElement(tab, 'input[name="phoneNumber"]')
+
+    await typeString(tab, "+353871112222")
+
+    await clickElement(tab, 'input[name="email"]')
+
+    await typeString(tab, "fake@gmail.com")
+
+    await clickElement(tab, 'input[name="re_email"]')
+
+    await typeString(tab, "fake@gmail.com")
+
+    await new Promise(r => setTimeout(r, 500));
+
+    await simulate_mouse_click(tab, 5, 5)
 
     await chrome.debugger.detach({ tabId: tab.id })
 
