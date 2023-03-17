484a1f117288b303209e5f4e5b8dfedad8d380d4
barryodev <barryodev@gmail.com>
2023-03-17 20:38:15 +0000

    basic proof of concept of finding an input field in a tab, getting it's x y, simulating a mouse click and key press on it

--- background.js
+++ background.js
@@ -1,13 +1,50 @@
-function changeBackgroundColor() {
-    document.body.style.backgroundColor = "blue";
-  }
-
-chrome.action.onClicked.addListener(function(tab) {
-    // No tabs or host permissions needed!
-    console.log('Turning ' + tab.url + ' blue!');
+chrome.action.onClicked.addListener(async function(tab) {
+    let full_doc, input_field, box_model, input_field_x, input_field_y
     
-    chrome.scripting.executeScript({
-        target : {tabId : tab.id},
-        func : changeBackgroundColor,
-      });
-  });
+    await chrome.debugger.attach({ tabId: tab.id }, "1.3")
+
+
+    full_doc = await chrome.debugger.sendCommand({ tabId: tab.id }, "DOM.getDocument", {
+        depth: -1
+    })
+    console.log('Got full doc node id: ' + full_doc.root.nodeId);
+
+    input_field = await chrome.debugger.sendCommand({tabId: tab.id }, "DOM.querySelector", {
+        nodeId: full_doc.root.nodeId,
+        selector: 'input[name="lastName"]'
+    })
+
+    console.log('Got node id of input field: ' + input_field);
+
+    box_model = await chrome.debugger.sendCommand({tabId: tab.id }, "DOM.getBoxModel", {
+        nodeId: input_field.nodeId
+    })
+
+    input_field_x = box_model.model.content[0] + 20
+    input_field_y = box_model.model.content[1] + 20
+
+    console.log('Box model of input field: ' + box_model.model.content);
+
+    await chrome.debugger.sendCommand({ tabId: tab.id }, "Input.dispatchMouseEvent", {
+        type: "mousePressed",
+        x: input_field_x,
+        y: input_field_y,
+        button: "left"
+    })
+
+    await chrome.debugger.sendCommand({ tabId: tab.id }, "Input.dispatchMouseEvent", {
+        type: "mouseReleased",
+        x: input_field_x,
+        y: input_field_y,
+        button: "left"
+    })
+
+    await chrome.debugger.sendCommand({ tabId: tab.id }, "Input.dispatchKeyEvent", {
+        type: "char",
+        text: "A",
+        modifiers: 0
+    })
+
+    await chrome.debugger.detach({ tabId: tab.id })
+
+});
